352e8982b6867d5feee37f37ae2839dbeb3d2de7
Kaushal Kishor <kaushal.kishor@niit.com>
2026-03-17 08:38:55 +0530

    project init

---
+++ springboot-async-playground/src/main/java/com/kkdevportal/springboot_async_playground/Application.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.springboot_async_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ springboot-async-playground/src/test/java/com/kkdevportal/springboot_async_playground/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.springboot_async_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
